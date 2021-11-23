9976b15c1dec9525b06f47f2c1422f2d0b0f53e8
Simeon Duwel <tameimpalafan123@gmail.com>
2021-11-23 15:33:53 +0100

    finally a working Display implementation

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,6 @@ use std::fmt;
 /// Due to the fact that this matrix is generic, most operations will not be defined properly.
 /// However, for all numeric types this works fine. This has the added benefit that you can define
 /// your own custom type and its corresponding operations, and it'll work out of the box.
-/// 
-/// The default type used in all of these constructors is f64. However, when supplying elements by
-/// hand, the type will be inferred automatically.
 
 
 
@@ -18,14 +15,49 @@ struct Matrix<T: Default> {
     contents: Vec<T>
 }
 
-impl<T: Default + fmt::Debug> fmt::Display for Matrix<T> {
+impl<T: 'static> fmt::Display for Matrix<T> where
+    T:  Default + fmt::Display,
+    &'static T: fmt::Display {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         //TODO: 1) get the length of the longest string representation
         //      2) right pad all other string reps
         //      3) insert a line break every time [columns] items have passed
         //      4) add unicode box drawing characters for the edges
-        let mut string_reps: Vec::<String> = Vec::with_capacity(self.contents.len());
-        write!(f, "under construction lmao")
+        let mut string_reps: Vec::<String> = vec![String::from(""); self.contents.len()];
+        let mut longest = 0;
+        for e in &self.contents {
+            let len = e.to_string().len();
+            if len > longest {
+                longest = len;
+            }
+        }
+
+        // now that we have the length of the longest element; get string reps
+        // and right-pad them with spaces to fit the length.
+
+        for (pos, e) in self.contents.iter().enumerate() {
+            string_reps[pos] = format!("{:>width$} ", *e, width = longest)
+        }
+
+        // we need to add two to accomodate for the top and bottom rows
+        let mut row_reps: Vec::<String> = vec![String::from(""); self.rows + 2]; 
+
+        for i in 0..self.rows + 2 {
+            
+            // top and bottom delimiters
+            if i == 0             {row_reps[i] = format!("{}{}{}", "┌ ", " ".repeat((longest + 1) * self.columns), "┐"); continue}
+            if i == self.rows + 1 {row_reps[i] = format!("{}{}{}", "└ ", " ".repeat((longest + 1) * self.columns), "┘"); continue}
+            
+            // remaining rows
+            //let content = &string_reps[(i-1)*self.columns .. (i-1) * self.columns].join("");
+            row_reps[i] = ["│ ", &string_reps[(i-1)*self.columns .. (i) * self.columns].join(""), "│"].join("");
+
+            //println!("row: {}", row_reps[i]);
+        }
+        
+        //println!("{:?}", row_reps);
+        //println!("{:?}", string_reps);
+        write!(f, "{}", row_reps.join("\n"))
     }
 }
 
@@ -68,5 +100,8 @@ impl<T: Default> Matrix<T> {
 
 
 fn main() {
-    
+    let m: Matrix<i32> = Matrix::new(3, 2, vec![1, 2, 3, 90000, 5]);
+    println!("{}", m);
+
+    println!("{}", Matrix::<f64>::ones(8, 8))
 }
